9caedd6995abe1a2a0c92fde4f7c4b8160ba2ee9
David <davidsusairaj1996@gmail.com>
2021-12-25 04:13:36 +0530

    mobile responsive navbar

--- src/component/nav.js
+++ src/component/nav.js
@@ -4,24 +4,38 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
 function Nav(params) {
   const [icon, setIcon] = useState(false);
-
+  const [formtxt, setFormtxt] = useState(false);
+  const [formtoggle, setFormtoggle] = useState(false);
   const menuClick = () => {
     setIcon(!icon);
+  };
+  const closeSideDrawer = () => {
+    setIcon(false);
+    setFormtxt(false);
+  };
+  const closeSideDrawer1 = () => {
+    setIcon(false);
+    setFormtxt(true);
+  };
+
+  const formstoggle = () => {
+    setFormtoggle(!formtoggle);
   };
 
   return (
     <div>
       <nav className="nav">
-        <div className="nav--logo">
+        <div className="nav--logo" onClick={closeSideDrawer}>
           <NavLink to="/" className="nav--logo__img">
             LOGO
           </NavLink>
         </div>
-        <div className="nav--menus">
+        <div className={icon ? "nav--menus show" : "nav--menus"}>
           <ul className="nav--menus--ul">
             <li className="nav--menus--ul__li">
               <NavLink
                 to="/"
+                onClick={closeSideDrawer}
                 className={(navData) => (navData.isActive ? "green" : "black")}
               >
                 HOME
@@ -30,6 +44,7 @@ function Nav(params) {
             <li className="nav--menus--ul__li">
               <NavLink
                 to="/about"
+                onClick={closeSideDrawer}
                 className={(navData) => (navData.isActive ? "green" : "black")}
               >
                 ABOUT
@@ -37,16 +52,37 @@ function Nav(params) {
             </li>
             <li className="nav--menus--ul__li">
               <div className="dropdown">
-                <p className="dropbtn">FORMS</p>
+                <p className={formtxt ? "formstxt" : "dropbtn"}>
+                  FORMS &nbsp;
+                  <i class="fas fa-angle-down" onClick={formstoggle}></i>
+                </p>
 
                 <div class="dropdown-content">
-                  <NavLink to="/registration" className="reg-link link">
+                  <NavLink
+                    to="/registration"
+                    className={(navData) =>
+                      navData.isActive ? "formactive" : "link"
+                    }
+                    onClick={closeSideDrawer1}
+                  >
                     REGISTRATION
                   </NavLink>
-                  <NavLink to="/login" className="login-link link">
+                  <NavLink
+                    to="/login"
+                    onClick={closeSideDrawer1}
+                    className={(navData) =>
+                      navData.isActive ? "formactive" : "link"
+                    }
+                  >
                     LOGIN
                   </NavLink>
-                  <NavLink to="/logout" className="logout-link link">
+                  <NavLink
+                    to="/logout"
+                    onClick={closeSideDrawer1}
+                    className={(navData) =>
+                      navData.isActive ? "formactive" : "link"
+                    }
+                  >
                     LOGOUT
                   </NavLink>
                 </div>
@@ -55,6 +91,7 @@ function Nav(params) {
             <li className="nav--menus--ul__li contact">
               <NavLink
                 to="/contact"
+                onClick={closeSideDrawer}
                 className={(navData) => (navData.isActive ? "green" : "black")}
               >
                 CONTACT
